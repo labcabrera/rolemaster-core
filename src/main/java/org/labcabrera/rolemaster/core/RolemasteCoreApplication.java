cc29fb099af3a2023608c9bfb7e5b7c174a624af
Luis Cabrera <lab.cabrera@gmail.com>
2022-03-18 12:58:56 +0100

    Initial version

---
+++ src/main/java/org/labcabrera/rolemaster/core/RolemasteCoreApplication.java
@@ -0,0 +1,13 @@
+package org.labcabrera.rolemaster.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolemasteCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolemasteCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/labcabrera/rolemaster/core/RolemasteCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.labcabrera.rolemaster.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolemasteCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
