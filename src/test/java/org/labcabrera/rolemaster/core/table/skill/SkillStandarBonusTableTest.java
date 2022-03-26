240cdcaf84ed456f0b9c04a5d12825055e925b46
Luis Cabrera <lab.cabrera@gmail.com>
2022-03-26 21:10:50 +0100

    Added standar skill bonus table

---
+++ src/test/java/org/labcabrera/rolemaster/core/table/skill/SkillStandarBonusTableTest.java
@@ -0,0 +1,61 @@
+package org.labcabrera.rolemaster.core.table.skill;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SkillStandarBonusTableTest {
+
+	private SkillStandarBonusTable table = new SkillStandarBonusTable();
+
+	@Test
+	void test00() {
+		assertEquals(-15, table.apply(0));
+	}
+
+	@Test
+	void test01() {
+		assertEquals(3, table.apply(1));
+	}
+
+	@Test
+	void test10() {
+		assertEquals(30, table.apply(10));
+	}
+
+	@Test
+	void test11() {
+		assertEquals(32, table.apply(11));
+	}
+
+	@Test
+	void test20() {
+		assertEquals(50, table.apply(20));
+	}
+
+	@Test
+	void test21() {
+		assertEquals(51, table.apply(21));
+	}
+
+	@Test
+	void test30() {
+		assertEquals(60, table.apply(30));
+	}
+
+	@Test
+	void test31() {
+		assertEquals(61, table.apply(31));
+	}
+
+	@Test
+	void test32() {
+		assertEquals(61, table.apply(32));
+	}
+
+	@Test
+	void test40() {
+		assertEquals(65, table.apply(40));
+	}
+
+}
