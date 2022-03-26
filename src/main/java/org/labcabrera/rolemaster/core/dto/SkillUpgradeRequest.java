59bbae8b13c20f4043dc19ed85da89cf3ed78452
Luis Cabrera <lab.cabrera@gmail.com>
2022-03-26 14:56:58 +0100

    Character upgrade skill service

--- src/main/java/org/labcabrera/rolemaster/core/dto/SkillUpgradeRequest.java
+++ src/main/java/org/labcabrera/rolemaster/core/dto/SkillUpgradeRequest.java
@@ -3,6 +3,8 @@ package org.labcabrera.rolemaster.core.dto;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +16,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class SkillUpgradeRequest {
 
+	@NotNull
 	@Builder.Default
 	private Map<String, Integer> categoryRanks = new LinkedHashMap<>();
 
+	@NotNull
 	@Builder.Default
 	private Map<String, Integer> skillRanks = new LinkedHashMap<>();
 
--- src/test/java/org/labcabrera/rolemaster/core/service/character/CharacterUpdateSkillServiceTest.java
+++ src/test/java/org/labcabrera/rolemaster/core/service/character/CharacterUpdateSkillServiceTest.java
@@ -3,6 +3,7 @@ package org.labcabrera.rolemaster.core.service.character;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Arrays;
@@ -15,11 +16,11 @@ import org.labcabrera.rolemaster.core.dto.SkillUpgradeRequest;
 import org.labcabrera.rolemaster.core.exception.BadRequestException;
 import org.labcabrera.rolemaster.core.model.character.CharacterDevelopment;
 import org.labcabrera.rolemaster.core.model.character.CharacterInfo;
+import org.labcabrera.rolemaster.core.model.character.CharacterSkill;
 import org.labcabrera.rolemaster.core.model.character.CharacterSkillCategory;
 import org.labcabrera.rolemaster.core.repository.CharacterInfoRepository;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -48,10 +49,16 @@ class CharacterUpdateSkillServiceTest {
 			.developmentCost(Arrays.asList(2, 7))
 			.upgradedRanks(1)
 			.build()));
+		character.setSkills(Arrays.asList(CharacterSkill.builder()
+			.skillId("s-01")
+			.developmentCost(Arrays.asList(1, 2, 3))
+			.upgradedRanks(6)
+			.build()));
 		character.setDevelopmentPoints(CharacterDevelopment.builder()
-			.remainingPoints(10)
+			.remainingPoints(20)
 			.build());
 		request.setCategoryRanks(Collections.singletonMap("cat-01", 2));
+		request.setSkillRanks(Collections.singletonMap("s-01", 3));
 		lenient().when(repository.findById(character.getId())).thenReturn(Mono.just(character));
 		lenient().when(repository.save(character)).thenReturn(Mono.just(character));
 	}
@@ -60,11 +67,14 @@ class CharacterUpdateSkillServiceTest {
 	void testSuccess() {
 		Mono<CharacterInfo> mono = service.updateRanks(character.getId(), request);
 		CharacterInfo result = mono.share().block();
+		assertEquals(5, result.getDevelopmentPoints().getRemainingPoints());
 		assertEquals(3, result.getSkillCategories().stream()
 			.filter(e -> e.getCategoryId().equals("cat-01")).findFirst().orElse(null)
 			.getUpgradedRanks());
-		assertEquals(1, result.getDevelopmentPoints().getRemainingPoints());
-		verify(repository, Mockito.times(1)).save(character);
+		assertEquals(9, result.getSkills().stream()
+			.filter(e -> e.getSkillId().equals("s-01")).findFirst().orElse(null)
+			.getUpgradedRanks());
+		verify(repository, times(1)).save(character);
 	}
 
 	@Test
@@ -73,7 +83,7 @@ class CharacterUpdateSkillServiceTest {
 			character.getDevelopmentPoints().setRemainingPoints(8);
 			service.updateRanks(character.getId(), request).share().block();
 		});
-		verify(repository, Mockito.times(0)).save(character);
+		verify(repository, times(0)).save(character);
 	}
 
 	@Test
@@ -82,7 +92,7 @@ class CharacterUpdateSkillServiceTest {
 			request.setCategoryRanks(Collections.singletonMap("cat-01", 3));
 			service.updateRanks(character.getId(), request).share().block();
 		});
-		verify(repository, Mockito.times(0)).save(character);
+		verify(repository, times(0)).save(character);
 	}
 
 	@Test
@@ -91,7 +101,7 @@ class CharacterUpdateSkillServiceTest {
 			request.setCategoryRanks(Collections.singletonMap("cat-02", 1));
 			service.updateRanks(character.getId(), request).share().block();
 		});
-		verify(repository, Mockito.times(0)).save(character);
+		verify(repository, times(0)).save(character);
 	}
 
 }
