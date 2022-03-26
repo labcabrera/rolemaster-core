e5ce45029d5b9084f779804e979b08899a9c0412
Luis Cabrera <lab.cabrera@gmail.com>
2022-03-26 22:17:28 +0100

    Fix test

--- src/test/java/org/labcabrera/rolemaster/core/service/character/CharacterCreationServiceTest.java
+++ src/test/java/org/labcabrera/rolemaster/core/service/character/CharacterCreationServiceTest.java
@@ -1,13 +1,17 @@
 package org.labcabrera.rolemaster.core.service.character;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.junit.jupiter.api.Test;
-import org.labcabrera.rolemaster.core.model.character.AttributeType;
 import org.labcabrera.rolemaster.core.model.character.CharacterInfo;
 import org.labcabrera.rolemaster.core.model.character.creation.CharacterCreationRequest;
 import org.labcabrera.rolemaster.core.repository.CharacterInfoRepository;
 import org.labcabrera.rolemaster.core.service.character.creation.CharacterCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -21,9 +25,12 @@ class CharacterCreationServiceTest {
 	@Autowired
 	private CharacterInfoRepository repository;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	void testCreation() {
-		CharacterCreationRequest request = createRequest();
+	void testCreation() throws IOException {
+		CharacterCreationRequest request = readRequest();
 
 		Mono<CharacterInfo> setUp = repository
 			.deleteAll()
@@ -35,27 +42,11 @@ class CharacterCreationServiceTest {
 			.verifyComplete();
 	}
 
-	private CharacterCreationRequest createRequest() {
-		CharacterCreationRequest result = CharacterCreationRequest.builder()
-			.name("Set")
-			.raceId("common-men")
-			.professionId("thief")
-			.age(41)
-			.weight(172)
-			.height(70)
-			.attributesRoll(660)
-			.build();
-		result.getBaseAttributes().put(AttributeType.AGILITY, 96);
-		result.getBaseAttributes().put(AttributeType.CONSTITUTION, 90);
-		result.getBaseAttributes().put(AttributeType.MEMORY, 38);
-		result.getBaseAttributes().put(AttributeType.REASONING, 43);
-		result.getBaseAttributes().put(AttributeType.SELF_DISCIPLINE, 39);
-		result.getBaseAttributes().put(AttributeType.EMPATHY, 20);
-		result.getBaseAttributes().put(AttributeType.INTUTITION, 90);
-		result.getBaseAttributes().put(AttributeType.PRESENCE, 50);
-		result.getBaseAttributes().put(AttributeType.STRENGTH, 92);
-		result.getBaseAttributes().put(AttributeType.QUICKNESS, 75);
-		return result;
+	private CharacterCreationRequest readRequest() throws IOException {
+		try (InputStream in = Thread.currentThread().getContextClassLoader()
+			.getResourceAsStream("openapi/examples/character-creation-example-01.json")) {
+			return objectMapper.readerFor(CharacterCreationRequest.class).readValue(in);
+		}
 	}
 
 }
