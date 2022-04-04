6ab266144f437c8bc340f9da7c5688688b5a481f
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-04 22:26:43 +0200

    Minor changes

--- src/main/java/org/labcabrera/rolemaster/core/dto/TacticalActionMeleeAttackDeclaration.java
+++ src/main/java/org/labcabrera/rolemaster/core/dto/TacticalActionMeleeAttackDeclaration.java
@@ -1,5 +1,24 @@
 package org.labcabrera.rolemaster.core.dto;
 
+import org.labcabrera.rolemaster.core.model.tactical.actions.MeleeAttackType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = true)
 public class TacticalActionMeleeAttackDeclaration extends TacticalActionDeclaration {
+
+	private MeleeAttackType meleeAttackType;
+
+	private String target;
+
+	private Integer parry;
 
 }
