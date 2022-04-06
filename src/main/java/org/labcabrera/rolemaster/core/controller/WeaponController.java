f9931087a11c6063987a042aac69717730c6b93d
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-06 23:39:40 +0200

    Initial data populator

--- src/main/java/org/labcabrera/rolemaster/core/controller/WeaponController.java
+++ src/main/java/org/labcabrera/rolemaster/core/controller/WeaponController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import reactor.core.publisher.Mono;
 
@@ -24,8 +25,8 @@ public interface WeaponController {
 	@GetMapping("/tables/{weaponId}/{armor}/{roll}")
 	@Operation(summary = "Gets the attack result of a specific weapon for a specific armor.")
 	Mono<String> findById(
-		@PathVariable("weaponId") String weaponId,
-		@PathVariable("armor") @Min(1) @Max(20) Integer armor,
-		@PathVariable("roll") @Min(1) @Max(150) Integer roll);
+		@Parameter(description = "Weapon identifier", required = true, example = "dagger") @PathVariable("weaponId") String weaponId,
+		@Parameter(description = "Amor (1-20)", required = true, example = "20") @PathVariable("armor") @Min(1) @Max(20) Integer armor,
+		@Parameter(description = "Roll (1-150)", required = true, example = "149") @PathVariable("roll") @Min(1) @Max(150) Integer roll);
 
 }
