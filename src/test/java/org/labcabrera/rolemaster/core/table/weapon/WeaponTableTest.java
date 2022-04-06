c744eb97e658afd1306b3daeeb9cb0e567230919
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-06 17:46:17 +0200

    Initial weapon structure table

---
+++ src/test/java/org/labcabrera/rolemaster/core/table/weapon/WeaponTableTest.java
@@ -0,0 +1,66 @@
+package org.labcabrera.rolemaster.core.table.weapon;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.labcabrera.rolemaster.core.exception.BadRequestException;
+
+class WeaponTableTest {
+
+	private WeaponTable weaponTable = new WeaponTable();
+
+	@Test
+	void test150() {
+		assertEquals("3CP", weaponTable.get("dagger", 150, 20));
+	}
+
+	@Test
+	void test114() {
+		assertEquals("3", weaponTable.get("dagger", 114, 20));
+	}
+
+	@Test
+	void test113() {
+		assertEquals("2", weaponTable.get("dagger", 113, 20));
+	}
+
+	@Test
+	void test100() {
+		assertEquals("2", weaponTable.get("dagger", 100, 20));
+	}
+
+	@Test
+	void test001() {
+		assertEquals("0", weaponTable.get("dagger", 1, 20));
+	}
+
+	@Test
+	void testInvalidRangeRoll01() {
+		assertThrows(BadRequestException.class, () -> {
+			weaponTable.get("dagger", 0, 20);
+		});
+	}
+
+	@Test
+	void testInvalidRangeRoll02() {
+		assertThrows(BadRequestException.class, () -> {
+			weaponTable.get("dagger", 151, 20);
+		});
+	}
+
+	@Test
+	void testInvalidRangeArmor01() {
+		assertThrows(BadRequestException.class, () -> {
+			weaponTable.get("dagger", 100, 21);
+		});
+	}
+
+	@Test
+	void testInvalidRangeArmor02() {
+		assertThrows(BadRequestException.class, () -> {
+			weaponTable.get("dagger", 100, 0);
+		});
+	}
+
+}
