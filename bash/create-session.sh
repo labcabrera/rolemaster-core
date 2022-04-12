58eada047122e67d16d9fcdd10b154638750026c
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-12 23:56:11 +0200

    Cleanup

--- bash/create-session.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -X 'POST' -s \
-  'http://localhost:8080/sessions?name=test-session' \
-  -H 'accept: application/json' \
-  | python3 -m json.tool
--- bash/find-character-by-id.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-URL="http://localhost:8080/characters/$1"
-
-curl -s -v \
-  $URL \
-  -H 'accept: application/json'
