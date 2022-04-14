780c465f3f1728624e73bcc3fb375fc3891956ac
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-14 22:20:21 +0200

    Critical table P

--- src/main/java/org/labcabrera/rolemaster/core/model/combat/ConditionalCriticalEffect.java
+++ src/main/java/org/labcabrera/rolemaster/core/model/combat/ConditionalCriticalEffect.java
@@ -8,13 +8,25 @@ public enum ConditionalCriticalEffect {
 
 	WITHOUT_LET_GREAVE("without-leg-greave"),
 
+	WITH_ARM_GREAVE("with-arm-greave"),
+
+	WITHOUT_ARM_GREAVE("without-arm-greave"),
+
 	WITH_HELMET("with-helmet"),
 
 	WITHOUT_HELMET("without-helmet"),
 
 	WITH_WAIST_ARMOR("with-waist-armor"),
 
-	WITHOUT_WAIST_ARMOR("without-waist-armor");
+	WITHOUT_WAIST_ARMOR("without-waist-armor"),
+
+	WITH_LEG_ARMOR("with-leg-armor"),
+
+	WITHOUT_LEG_ARMOR("without-leg-armor"),
+	
+	WITH_PLATE_CHEST("with-plate-chest"),
+	
+	WITHOUT_PLATE_CHEST("without-plate-chest");
 
 	private String name;
 
