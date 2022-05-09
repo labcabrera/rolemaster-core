575bffc6b43043b9cd1e51a32878b6d25a4b957f
Luis Cabrera <lab.cabrera@gmail.com>
2022-05-09 19:34:06 +0200

    Added power point dev skill. Fix sorcerer profession costs.

--- src/main/java/org/labcabrera/rolemaster/core/service/populator/SkillPopulator.java
+++ src/main/java/org/labcabrera/rolemaster/core/service/populator/SkillPopulator.java
@@ -27,6 +27,7 @@ public class SkillPopulator extends AbstractJsonPopulator<Skill> {
 			"data/populator/skills/skills-martial-arts.json",
 			"data/populator/skills/skills-outdoor-animal.json",
 			"data/populator/skills/skills-outdoor-environmental.json",
+			"data/populator/skills/skills-power-point-development.json",
 			"data/populator/skills/skills-science.json",
 			"data/populator/skills/skills-self-control.json",
 			"data/populator/skills/skills-special-attacks.json",
