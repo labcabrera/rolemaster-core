4ee7867fe469bb6f3e78283ec578ba1c622ff246
Luis Cabrera <lab.cabrera@gmail.com>
2022-05-11 16:49:07 +0200

    Added wanderer training package.

--- src/main/java/org/labcabrera/rolemaster/core/service/populator/TrainingPackagePopulator.java
+++ src/main/java/org/labcabrera/rolemaster/core/service/populator/TrainingPackagePopulator.java
@@ -47,6 +47,7 @@ class TrainingPackagePopulator extends AbstractJsonPopulator<TrainingPackage> {
 			"data/populator/training/training-package-soldier.json",
 			"data/populator/training/training-package-spy.json",
 			"data/populator/training/training-package-traveler.json",
+			"data/populator/training/training-package-wanderer.json",
 			"data/populator/training/training-package-weapon-master.json",
 			"data/populator/training/training-packages-character-law.json");
 	}
