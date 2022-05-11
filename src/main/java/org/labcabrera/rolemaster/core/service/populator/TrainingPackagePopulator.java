5191f2a9fe6e29f79ae0cb56f489940d53fbab26
Luis Cabrera <lab.cabrera@gmail.com>
2022-05-11 12:40:11 +0200

    Added training package doctor.

--- src/main/java/org/labcabrera/rolemaster/core/service/populator/TrainingPackagePopulator.java
+++ src/main/java/org/labcabrera/rolemaster/core/service/populator/TrainingPackagePopulator.java
@@ -20,6 +20,7 @@ public class TrainingPackagePopulator extends AbstractJsonPopulator<TrainingPack
 			"data/populator/training/training-package-burglar.json",
 			"data/populator/training/training-package-city-guard.json",
 			"data/populator/training/training-package-crafter.json",
+			"data/populator/training/training-package-doctor.json",
 			"data/populator/training/training-package-explorer.json",
 			"data/populator/training/training-package-herbalist.json",
 			"data/populator/training/training-package-highwayman.json",
